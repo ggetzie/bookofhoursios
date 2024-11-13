c73044877b80fc37d22c924a3eee8e557d59e570
Gabriel Getzie <gabe@kotsf.com>
2024-11-13 09:24:37 +0800

    Initial Commit

---
+++ BookOfHoursTests/BookOfHoursTests.swift
@@ -0,0 +1,17 @@
+//
+//  BookOfHoursTests.swift
+//  BookOfHoursTests
+//
+//  Created by Gabriel Getzie on 11/13/24.
+//
+
+import Testing
+@testable import BookOfHours
+
+struct BookOfHoursTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
